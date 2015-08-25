0c4c7f9c8a24fee7d439cc342a5e24886edf0ce4
qasim-online-now <u_cant_see_me6x6@yahoo.com>
2015-08-25 19:41:00 +0500

    working on ex 2.5

---
+++ exTwoPointFive.java
@@ -0,0 +1,25 @@
+/*  solving exercise 2.5
+    the following code is for simple calculator
+*/
+// java library importer block start
+import java.util.Scanner;
+//java library importer block end
+
+public class exTwoPointFive {// class body start
+
+    // main function or the entering point
+    public static void main() {//main function body start
+
+        //part a) State that a program will calculate the product of three integers.
+        //The program will calculate the product of three integers.
+
+        //part b) Create a Scanner called input that reads values from the standard input.
+
+        Scanner input = new Scanner(System.in);
+
+
+
+    }// main function body end
+
+
+}// class body end
