539fc21eda921665c0c852a8b06714bdd6247b85
qasim-rafique <qasim.rafique@virtism.com>
2015-08-27 18:57:07 -0700

    chapter 3 till fig 3.2

---
+++ ch3/GradeBookTest.java
@@ -0,0 +1,19 @@
+// fig 3.2 GradeBookTest.java
+/* 
+	this class use for creating the object of
+	GradeBook and use it's method displayMassage
+*/
+
+public class GradeBookTest {// class body start
+	
+	public static void main() {// main methd body start
+		
+		// creating object of GradeBook
+		GradeBook GradeObj = new GradeBook();
+		
+		// calling mathod displymethod from GradeBook
+		GradeObj.displayMassage();
+		
+	}// main method body end
+	
+} // class body end
